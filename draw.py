e7359035c2183e57a60ceddd8d43bc4c05e332a0
bayertom <bayertom@natur.cuni.cz>
2022-02-21 11:45:03 +0100

    cv1

--- draw.py
+++ draw.py
@@ -1,5 +1,3 @@
-import select
-
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
@@ -7,37 +5,3 @@ from PyQt6.QtWidgets import *
 class Draw(QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.pol = QPolygon()
-
-    def mousePressEvent(self, e:QMouseEvent):
-        #Get position
-        x = e.position().x()
-        y = e.position().y()
-
-        #Create new polygon
-        p = QPoint(x, y)
-
-        #Add polygon to the list
-        self.pol.append(p)
-
-        #Repaint
-        self.repaint()
-
-    def paintEvent(self, e: QPaintEvent):
-
-        #Create graphic object
-        qp = QPainter(self)
-
-        #Set parameters
-        qp.setPen(Qt.GlobalColor.red)
-        qp.setBrush(Qt.GlobalColor.yellow)
-
-        #Start draw
-        qp.begin(self)
-        qp.drawPolygon(self.pol)
-
-        #End draw
-        qp.end()
-
-    def getPol(self):
-        return self.pol
